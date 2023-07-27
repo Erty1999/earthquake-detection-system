5ec12bf8aee54b232b5f572f5b2e1608b6f205c3
marco <marcocabras99@gmail.com>
2023-07-27 12:59:20 +0200

    x

--- passiveDevicesManager/src/index.ts
+++ passiveDevicesManager/src/index.ts
@@ -27,11 +27,11 @@ async function main() {
   // routes
   app.use(router);
 
-    //config dotenv
-    config();
-  
-    // port
-    const port = process.env.PORT;
+  //config dotenv
+  config();
+
+  // port
+  const port = process.env.PORT;
 
   app.listen(port, () => {
     console.log("Started on port " + port);
